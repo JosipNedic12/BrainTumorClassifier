4bc332f426d12d86b28007632dfe06558900f457
JosipNedic <josip.nedic.jn@gmail.com>
2025-05-06 12:33:20 +0200

    Initial commit

---
+++ Models/ChartJsPieChartData.cs
@@ -0,0 +1,8 @@
+﻿namespace BrainTumorClassifier.Models
+{
+    public class ChartJsPieChartData
+    {
+        public string[] Labels { get; set; } = Array.Empty<string>();
+        public double[] Data { get; set; } = Array.Empty<double>();
+    }
+}
---
+++ Models/PredictionResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BrainTumorClassifier.Models
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    public class PredictionResult
+    {
+        [JsonPropertyName("predicted_label")]
+        public string PredictedLabel { get; set; }
+
+        [JsonPropertyName("scores")]
+        public Dictionary<string, float> Scores { get; set; }
+    }
+}
